8a2d3ff5344a28f87867226f71ed0002a7a044ac
wangtao <ytzojehjpvozc@outlook.com>
2022-04-29 00:22:59 +0800

    :feat: 添加knife4j 生成接口文档

--- common/src/main/java/com/xbh/politemic/common/config/Knife4jConfig.java
+++ common/src/main/java/com/xbh/politemic/common/config/Knife4jConfig.java
@@ -1,74 +1,88 @@
 package com.xbh.politemic.common.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.util.ClassUtils;
 import springfox.documentation.RequestHandler;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Optional;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
+import static java.util.Optional.ofNullable;
+
 /**
- * @Knife4jConfig: 接口文档 配置
+ * @Knife4jConfig: knife4j 配置
  * @author: ZBoHang
- * @time: 2021/11/24 9:15
+ * @time: 2021/12/9 9:36
  */
+@Slf4j
 @Configuration
-//@EnableKnife4j
-@EnableSwagger2WebMvc
+@EnableSwagger2
 public class Knife4jConfig {
 
+    /**
+     * 配置knife4j
+     */
     @Bean(value = "defaultApi2")
     public Docket defaultApi2() {
         Docket docket=new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(this.apiInfo())
                 //分组名称
-                .groupName("POLITEMIC-1.X版本")
+                .groupName("1.X版本")
                 .select()
                 //这里指定Controller扫描包路径
-                .apis(RequestHandlerSelectors.basePackage("com.xbh.politemic.biz.user.controller"))
+                .apis(basePackage("com.xbh.politemic"))
                 .paths(PathSelectors.any())
                 .build();
         return docket;
     }
 
     /**
-     * 构建 apiInfo
-     * @author: ZBoHang
-     * @time: 2021/12/8 15:30
+     * 构建apiInfo
      */
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-                //.title("swagger-bootstrap-ui-demo RESTful APIs")
-                .description("# Politemic RESTful APIs")
-                .contact(new Contact("zhengbohang", "https://gitee.com/zheng-bohang/politemic", "161078369@qq.com"))
+                .title("POLITEMIC RESTful APIs")
+                .description("# POLITEMIC RESTful APIs")
+                // .termsOfServiceUrl("http://www.xx.com/")
+                .contact(new Contact("XBH", "https://gitee.com/zheng-bohang/politemic", "161078369@qq.com"))
                 .version("1.0")
                 .build();
     }
 
-    public static Predicate<RequestHandler> basePackage(final String basePackage) {
-        return (input) -> {
-            return (Boolean)declaringClass(input).map(handlerPackage(basePackage)).orElse(true);
-        };
-    }
-
-    private static Optional<? extends Class<?>> declaringClass(RequestHandler input) {
-        return Optional.ofNullable(input.declaringClass());
-    }
-
     private static Function<Class<?>, Boolean> handlerPackage(final String basePackage) {
 
-        return (input) -> {
-            return ClassUtils.getPackageName(input).startsWith(basePackage);
+        return new Function<Class<?>, Boolean>() {
+            @Override
+            public Boolean apply(Class<?> input) {
+                String packageName = ClassUtils.getPackageName(input);
+                log.info("knife4j扫描 -->>> " + input.getSimpleName());
+                return packageName.startsWith(basePackage);
+            }
         };
+        // return input -> ClassUtils.getPackageName(input).startsWith(basePackage);
+    }
+
+    public static Predicate<RequestHandler> basePackage(String basePackage) {
+        return new Predicate<RequestHandler>() {
+            @Override
+            public boolean test(RequestHandler input) {
+                return declaringClass(input).map(handlerPackage(basePackage)).orElse(true);
+            }
+        };
+        // return input -> declaringClass(input).map(handlerPackage(basePackage)).orElse(true);
+    }
+
+    private static Optional<Class<?>> declaringClass(RequestHandler input) {
+        return ofNullable(input.declaringClass());
     }
 }
--- politemic/src/main/java/com/xbh/politemic/biz/user/controller/UserController.java
+++ politemic/src/main/java/com/xbh/politemic/biz/user/controller/UserController.java
@@ -1,19 +1,26 @@
 package com.xbh.politemic.biz.user.controller;
 
+import com.xbh.politemic.biz.user.domain.SysUser;
 import com.xbh.politemic.biz.user.srv.UserSrv;
 import com.xbh.politemic.common.annotation.NoneNeedLogin;
 import com.xbh.politemic.common.annotation.SysLog;
 import com.xbh.politemic.common.constant.Constants;
-import com.xbh.politemic.biz.user.domain.SysUser;
 import com.xbh.politemic.common.util.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 /**
  * @Description: 用户控制器
  * @Author: zhengbohang
  * @Date: 2021/10/3 13:45
  */
+@Api("用户模块")
 @RestController
 @RequestMapping("/user/user")
 public class UserController {
@@ -22,13 +29,21 @@ public class UserController {
     private UserSrv userSrv;
 
     @NoneNeedLogin
+    @ApiImplicitParam(name = "test",value = "测试参数",required = true)
+    @ApiOperation(value = "测试接口")
     @PostMapping("test")
-    @SysLog(modelName = Constants.USER_MODEL_NAME, behavior = "用于测试", remark = "test可选")
+    @SysLog(modelName = Constants.USER_MODEL_NAME, behavior = "用于测试", remark = "remark")
     public Result test(@RequestParam(name = "test") String test) throws Exception {
-        return Result.success("123321");
+        String solvedData = Optional.ofNullable(test).orElse("空的test参数");
+        return Result.success(solvedData);
     }
 
     @NoneNeedLogin
+    @ApiImplicitParams({
+                    @ApiImplicitParam(name = "userName", value = "用户名", required = false),
+                    @ApiImplicitParam(name = "userPass", value = "密码", required = false)
+    })
+    @ApiOperation(value = "用户登录接口")
     @PostMapping("userLogin")
     @SysLog(modelName = Constants.USER_MODEL_NAME, behavior = "用户登录", remark = "日志中密码替换")
     public Result userLogin(@RequestParam(name = "userName", required = false) String userName,
@@ -37,6 +52,7 @@ public class UserController {
     }
 
     @NoneNeedLogin
+    @ApiOperation(value = "用户注册接口")
     @PostMapping("userRegister")
     @SysLog(modelName = Constants.USER_MODEL_NAME, behavior = "用户注册", remark = "只允许普通用户进行注册")
     public Result userRegister(@RequestBody(required = false) SysUser sysUser) {
@@ -44,6 +60,11 @@ public class UserController {
     }
 
     @NoneNeedLogin
+    @ApiOperation(value = "用户激活接口")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "id", value = "用户id", paramType = "path"),
+            @ApiImplicitParam(name = "activateCode", value = "激活码", paramType = "path")
+    })
     @GetMapping("userActivate/{id}/{activateCode}")
     @SysLog(modelName = Constants.USER_MODEL_NAME, behavior = "激活账户", remark = "普通用户激活")
     public Result userActivate(@PathVariable(name = "id", required = false) String id,
